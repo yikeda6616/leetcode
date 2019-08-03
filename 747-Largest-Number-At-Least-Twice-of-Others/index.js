38bd8a5b5c4ce75fedf32ee0612ebdca1705c930
Yasushi Ikeda <yikeda6616@gmail.com>
2019-08-03 15:13:25 +0900

    Add daily

--- 747-Largest-Number-At-Least-Twice-of-Others/index.js
+++ 747-Largest-Number-At-Least-Twice-of-Others/index.js
@@ -3,22 +3,24 @@
  * @return {number}
  */
 function dominantIndex(nums) {
-  let [max, maxIndex, second] = [0, 0, 0];
-
-  nums.forEach((num, index) => {
-    if (max <= num) {
-      max = num;
-      maxIndex = index;
-    } else if (second <= num) {
-      second = num;
+  let max = -Infinity;
+  let second = -Infinity;
+  let maxIndex = 0;
+  for (const [i, n] of nums.entries()) {
+    if (n > max) {
+      second = max;
+      max = n;
+      maxIndex = i;
+    } else if (n > second) {
+      second = n;
     }
-  });
-  console.log(max, second, maxIndex);
-  return max >= second * 2 ? maxIndex : -1;
+  }
+  if (max >= second * 2) {
+    return maxIndex;
+  }
+  return -1;
 }
 
 // const testCases = [[0, 0, 2, 3], [1], [0, 0, 0, 1], [3, 6, 1, 0], [1, 2, 3, 4]];
 
 // testCases.forEach(item => dominantIndex(item));
-
-console.log(dominantIndex([0, 0, 2, 3]));
