2a8a6fe35ead434de6e8c28e2e673271d43bd2c6
Yasushi Ikeda <yikeda6616@gmail.com>
2020-04-02 12:41:36 +0900

    chore: Archive old files

--- 001-TwoSum/twosum.test.js
+++
@@ -1,7 +0,0 @@
-import { twoSum } from './twosum';
-
-describe('twoSum()', () => {
-  test('[2, 7, 11, 15]', () => {
-    expect(twoSum([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
-  });
-});
--- String-Compression/string-compression.test.js
+++
@@ -1,7 +0,0 @@
-import { compression } from './string-compression';
-
-describe('string-compression', () => {
-  test('aaabbcccc is a3b2c4', () => {
-    expect(compression('aaabbcccc')).toBe('a3b2c4');
-  });
-});
