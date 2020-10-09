3dca14ce4c1277f5616583905e11b4f2061837e7
Yasushi Ikeda <yikeda6616@gmail.com>
2020-10-09 19:52:50 +0900

    Add 1470. Shuffle the Array

---
+++ algorithm/1470. Shuffle the Array/test.js
@@ -0,0 +1,28 @@
+const { shuffle } = require('.');
+
+const name = 'shuffle';
+const tests = [
+  {
+    nums: [2, 5, 1, 3, 4, 7],
+    n: 3,
+    result: [2, 3, 5, 4, 1, 7],
+  },
+  {
+    nums: [1, 2, 3, 4, 4, 3, 2, 1],
+    n: 4,
+    result: [1, 4, 2, 3, 3, 2, 4, 1],
+  },
+  {
+    nums: [1, 1, 2, 2],
+    n: 2,
+    result: [1, 2, 1, 2],
+  },
+];
+
+describe(`#${name})`, () => {
+  for (const i of tests) {
+    test(`${name}(${i.nums}, ${i.n}) should return ${i.result}`, () => {
+      expect(shuffle(i.nums, i.n)).toStrictEqual(i.result);
+    });
+  }
+});
