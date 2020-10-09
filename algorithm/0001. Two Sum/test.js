886a333427799e111a50c50c7330bfce7d513e33
Yasushi Ikeda <yikeda6616@gmail.com>
2020-10-09 19:51:56 +0900

    Update 0001. Two Sum

---
+++ algorithm/0001. Two Sum/test.js
@@ -0,0 +1,28 @@
+const { twoSum } = require('.');
+
+const name = 'twoSum';
+const tests = [
+  {
+    nums: [2, 7, 11, 15],
+    n: 9,
+    result: [0, 1],
+  },
+  {
+    nums: [3, 2, 4],
+    n: 6,
+    result: [1, 2],
+  },
+  {
+    nums: [3, 3],
+    n: 6,
+    result: [0, 1],
+  },
+];
+
+describe(`#${name})`, () => {
+  for (const i of tests) {
+    test(`${name}(${i.nums}, ${i.n}) should return ${i.result}`, () => {
+      expect(twoSum(i.nums, i.n)).toStrictEqual(i.result);
+    });
+  }
+});
