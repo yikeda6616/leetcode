2237867f3f60149235bd3c8a13b8834237f7c4ed
Yasushi Ikeda <yikeda6616@gmail.com>
2021-02-26 20:30:49 +0900

    feat: setLanguageProcess

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { LeetCode } from './scraper';
 (async () => {
     const lg = new LeetCode();
     await lg.initialize();
+    await lg.setLanguageProcess();
     await lg.accessPageProcess();
     await lg.createDirProcess();
     await lg.getContentProcess();
