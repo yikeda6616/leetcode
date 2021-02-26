b4dad38314315436b1c95c05e66ece2385459ff0
Yasushi Ikeda <yikeda6616@gmail.com>
2021-02-27 07:48:42 +0900

    prettier: tab size 2

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import { LeetCode } from './scraper';
 
 (async () => {
-    const lg = new LeetCode();
-    await lg.initialize();
-    await lg.setLanguageProcess();
-    await lg.accessPageProcess();
-    await lg.createDirProcess();
-    await lg.getContentProcess();
-    // TODO: await lg.changeLanguageProcess();
-    // await lg.close();
+	const lg = new LeetCode();
+	await lg.initialize();
+	await lg.setLanguageProcess();
+	await lg.accessPageProcess();
+	await lg.createDirProcess();
+	await lg.getContentProcess();
+	await lg.getTemplateProcess();
+	// TODO: await lg.changeLanguageProcess();
+	// await lg.close();
 })();
--- scraper/index.ts
+++ scraper/index.ts
@@ -5,55 +5,59 @@ import { createDir } from './util';
 const URL = 'https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/';
 
 export class LeetCode {
-    browser: Browser | null = null;
-    page: Page | null = null;
-    dirName: string | undefined = "";
-    async initialize() {
-        this.browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 50,
-        })
-        this.page = await this.browser?.newPage();
-    }
+	browser: Browser | null = null;
+	page: Page | null = null;
+	dirName: string | undefined = "";
+	async initialize() {
+		this.browser = await puppeteer.launch({
+			headless: false,
+			slowMo: 50,
+		})
+		this.page = await this.browser?.newPage();
+	}
 
-    async setLanguageProcess() {
-        await this.page?.goto(URL);
-        await this.page?.evaluate((language) => {
-            localStorage.setItem('global_lang', language);
-        }, 'typescript')
-    }
+	async setLanguageProcess() {
+		await this.page?.goto(URL);
+		await this.page?.evaluate((language) => {
+			localStorage.setItem('global_lang', language);
+		}, 'typescript')
+	}
 
-    async accessPageProcess() {
-        await this.page?.goto(URL, { waitUntil: 'networkidle2' });
-    }
+	async accessPageProcess() {
+		await this.page?.goto(URL, { waitUntil: 'networkidle2' });
+	}
 
-    async createDirProcess() {
-        const selector = '[data-cy="question-title"]'
-        const element = await this.page?.$(selector);
-        this.dirName = await (await element?.getProperty('textContent'))?.jsonValue();
-        await createDir(this.dirName, './algorithm');
-    }
+	async createDirProcess() {
+		const selector = '[data-cy="question-title"]'
+		const element = await this.page?.$(selector);
+		this.dirName = await (await element?.getProperty('textContent'))?.jsonValue();
+		await createDir(this.dirName, './algorithm');
+	}
 
-    async getContentProcess() {
-        const selector = 'div[class*="question-content"]';
-        const element = await this.page?.$(selector);
-        const html = await (await element?.getProperty('innerHTML'))?.jsonValue();
+	async getContentProcess() {
+		const selector = 'div[class*="question-content"]';
+		const element = await this.page?.$(selector);
+		const html = await (await element?.getProperty('innerHTML'))?.jsonValue();
 
-        const markdown = NodeHtmlMarkdown.translate(html as any);
-        const fs = require('fs');
-        const filePath = `./algorithm/${this.dirName}/index.md`
-        fs.writeFile(filePath, markdown, (err: any) => {
-            if (err) throw err;
-            console.log(`${filePath} is created successfully.`);
-        })
-        // await createFile(`./algorithm/${this.dirName}/`, content, (err) => {
-        //     return console.log(err);
-        // });
-    }
+		const markdown = NodeHtmlMarkdown.translate(html as any);
+		const fs = require('fs');
+		const filePath = `./algorithm/${this.dirName}/index.md`
+		fs.writeFile(filePath, markdown, (err: any) => {
+			if (err) throw err;
+			console.log(`${filePath} is created successfully.`);
+		})
+		// await createFile(`./algorithm/${this.dirName}/`, content, (err) => {
+		//     return console.log(err);
+		// });
+	}
 
-    // TODO: async changeLanguageProcess() {}
+	async getTemplateProcess() {
+		const selector = '.CodeMirror-code';
+		const element = await this.page?.$(selector);
+		console.log(element);
+	}
 
-    async close() {
-        await this.browser?.close();
-    }
+	async close() {
+		await this.browser?.close();
+	}
 }
